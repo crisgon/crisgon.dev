81f072ef43e70ba31411f01342e2a83b7c9afdf5
Cristiano <cristiano.goncalves@cubos.io>
2019-08-15 23:10:59 -0300

    refactor: add span and role on footer emoji.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,5 +3,9 @@ import React from "react";
 import * as S from "./styled";
 
 export default function Footer() {
-	return <S.Footer>Desenvolvido com ❤️ usando Gatsby</S.Footer>;
+	return (
+		<S.Footer>
+			Desenvolvido com <span role="img">❤️</span> usando Gatsby
+		</S.Footer>
+	);
 }
